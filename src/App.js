7ee98119b5421a92845ebe64479b4a0d24f7565f
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2022-08-15 19:42:53 +0530

    Fixes

--- src/App.js
+++ src/App.js
@@ -20,75 +20,54 @@ const App = () => {
           shadow={true}
         />
         <Routes>
-          <Route exact path="/">
-            <News
-              setProgress={setProgress}
-              key="general"
-              pageSize={pageSize}
-              category="general"
-              apikey={apikey}
-            ></News>
-          </Route>
+          <Route exact path="/" element={<News
+            setProgress={setProgress}
+            key="general"
+            pageSize={pageSize}
+            category="general"
+            apikey={apikey} />} />
           <Route path="general" element={<News
-              setProgress={setProgress}
-              key="general"
-              pageSize={pageSize}
-              category="general"
-              apikey={apikey} />}/>
-          <Route path="business">
-            <News
-              setProgress={setProgress}
-              key="business"
-              pageSize={pageSize}
-              category="business"
-              apikey={apikey}
-            ></News>
-          </Route>
-          <Route path="entertainment">
-            <News
-              setProgress={setProgress}
-              key="entertainment"
-              pageSize={pageSize}
-              category="entertainment"
-              apikey={apikey}
-            ></News>
-          </Route>
-          <Route path="health">
-            <News
-              setProgress={setProgress}
-              key="health"
-              pageSize={pageSize}
-              category="health"
-              apikey={apikey}
-            ></News>
-          </Route>
-          <Route path="science">
-            <News
-              setProgress={setProgress}
-              key="science"
-              pageSize={pageSize}
-              category="science"
-              apikey={apikey}
-            ></News>
-          </Route>
-          <Route path="sports">
-            <News
-              setProgress={setProgress}
-              key="sports"
-              pageSize={pageSize}
-              category="sports"
-              apikey={apikey}
-            ></News>
-          </Route>
-          <Route path="technology">
-            <News
-              setProgress={setProgress}
-              key="technology"
-              pageSize={pageSize}
-              category="technology"
-              apikey={apikey}
-            ></News>
-          </Route>
+            setProgress={setProgress}
+            key="general"
+            pageSize={pageSize}
+            category="general"
+            apikey={apikey} />} />
+          <Route path="business" element={<News
+            setProgress={setProgress}
+            key="business"
+            pageSize={pageSize}
+            category="business"
+            apikey={apikey} />} />
+          <Route path="entertainment" element={<News
+            setProgress={setProgress}
+            key="entertainment"
+            pageSize={pageSize}
+            category="entertainment"
+            apikey={apikey} />} />
+          <Route path="health" element={<News
+            setProgress={setProgress}
+            key="health"
+            pageSize={pageSize}
+            category="health"
+            apikey={apikey} />} />
+          <Route path="science" element={<News
+            setProgress={setProgress}
+            key="science"
+            pageSize={pageSize}
+            category="science"
+            apikey={apikey} />} />
+          <Route path="sports" element={<News
+            setProgress={setProgress}
+            key="sports"
+            pageSize={pageSize}
+            category="sports"
+            apikey={apikey} />} />
+          <Route path="technology" element={<News
+            setProgress={setProgress}
+            key="technology"
+            pageSize={pageSize}
+            category="technology"
+            apikey={apikey} />} />
         </Routes>
       </BrowserRouter>
     </div>
